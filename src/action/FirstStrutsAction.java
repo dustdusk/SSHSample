d75e66ff83fcad189ccc79e24b18f5ede3c0ad91
dustdusk <dustdusk@gmail.com>
2015-05-21 18:04:50 +0800

    complete transaction manager

--- src/action/FirstStrutsAction.java
+++ src/action/FirstStrutsAction.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import service.FirstSpringService;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -18,6 +21,7 @@ public class FirstStrutsAction extends ActionSupport{
 	private static final long serialVersionUID = 1L;
 
 	public String reExcute(){
+		@SuppressWarnings("unused")
 		List<SshMasterTable> SshMasterTableList = firstSpringService.querySSHMasterTable();
 		
 		return "firstStrutsPageA";
--- src/dao/FirstSpringDao.java
+++ src/dao/FirstSpringDao.java
@@ -5,30 +5,32 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
-import entity.SshMasterTable;
-
+@Transactional
+@EnableTransactionManagement
 public class FirstSpringDao<T> {
 	//使用Annotation的方式取得Spring中設定好的Bean
 	@Resource
 	private SessionFactory sessionFactory = null;
 
     
-	@SuppressWarnings("unchecked")
 	public List<T> query(T entity ){
 		return query(entity, false);
 	}
+
 	
-	@SuppressWarnings("unchecked")
 	public List<T> query(T entity, Boolean newSession ){
 		Criteria criteria = null;
 		if(newSession){
 			criteria = sessionFactory.openSession().createCriteria(entity.getClass());
 		} else {
 			//使用Spring做trancation控管
-			criteria = sessionFactory.getCurrentSession().createCriteria(entity.getClass());
+			Session session = sessionFactory.getCurrentSession();
+			criteria = session.createCriteria(entity.getClass());
 		}
 		//criteria.add(Restrictions.eq("", "1")); //加入查詢條件
 		return criteria.list();
